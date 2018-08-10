5388ed5b384d7f032e757afa4ad2652b4bfeb5b1
Hien <caoduonghien@gmail.com>
2018-08-10 11:18:48 +0300

    Heading sematics

--- js/main.js
+++ js/main.js
@@ -184,7 +184,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = restaurant.name + ' restaurant image';
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
